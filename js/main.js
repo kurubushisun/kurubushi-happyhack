31239f951e5852510bc0bb821c56516ac4677ca9
kurubushisun <as0129@keio.jp>
2026-09-14 01:48:08 +0900

    firs commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function($){
+    $('.firstview').bgSwitcher({
+        images:['images/background1.jpg','images/background2.jpg','images/background3.jpg','images/background4.jpg','images/background5.jpg',],
+        interval: 7000,
+        loop: true,
+    });
+    particlesJS.load(
+        'particles-js',
+        'js/particlesjs-config.json'
+    );
+
+    $('.slider').slick({
+        autoplay: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+    });
+});
